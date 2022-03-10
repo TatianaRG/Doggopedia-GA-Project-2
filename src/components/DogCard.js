96dc056a12060b07308a627e57f5dde433fcacfb
TatianaGuzun <tatiana.guzun@reachplc.com>
2022-03-10 14:38:52 +0000

    added extra css

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -12,17 +12,19 @@ const DogCard = ({
 }) => {
   //console.log(name);
   return (
-    <div className='column $tile-header-shadow is-one-quarter-desktop is-one-third-tablet'>
+    <div className="column $tile-header-shadow is-one-quarter-desktop is-one-third-tablet">
       <Link to={`/dogs/${name.toLowerCase()}`}>
         {/* <div className='card'>
           <div className='card-header'>
             <h2 className='card-header-title'>{name}</h2>
           </div> */}
-        <div className='tile is-parent'>
-          <article className='tile is-child notification is-warning'>
-            <p className='title'>{name}</p>
-            <figure className='image is-5by5'>
-              <img id='doggo' src={image.url} alt={name} />
+        <div className="tile is-parent">
+          <article className="tile is-child notification is-grey">
+            <p id="card-title" className="title is-4">
+              {name}
+            </p>
+            <figure className="image is-5by5">
+              <img id="doggo" src={image.url} alt={name} />
             </figure>
           </article>
         </div>
--- src/components/DogShow.js
+++ src/components/DogShow.js
@@ -13,7 +13,8 @@ const DogShow = () => {
       try {
         const { data } = await getSingleDog(point); /// using axios package. data is the key that we are using from API
         // setDog(data);
-        console.log(data);
+        // console.log(data[0]);
+        setDog(data[0]);
       } catch (err) {
         console.error(err);
       }
@@ -22,33 +23,36 @@ const DogShow = () => {
   }, [point]);
 
   return (
-    <section className="section">
+    <section className="section1">
       <div className="container">
         {!dog ? (
           <p>Loading...</p>
         ) : (
           <div>
-            <h2 className="title has-text-centered">{dog.name}</h2>
+            <h2 className="title has-text-centered">Name: {dog.name}</h2>
             <hr />
             <div className="columns">
-              <div className="column is-half">
-                <figure className="image">
-                  <img src={dog.image} alt={dog.name} />
+              <div className="column is-8">
+                <figure className="image is-4by4">
+                  <img
+                    src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}
+                    alt={dog.name}
+                  />
                 </figure>
               </div>
-              <div className="column is-half">
-                <h4 className="title is-8">Breed Group:</h4>
-                <hr />
-                <p>{dog.breed_group}</p>
-                <hr />
-                <h4 className="title is-4">Origin:</h4>
-                <hr />
-                <p>{dog.origin}</p>
-                <h4 className="title is-4">Lifespan:</h4>
-                <hr />
-                <p>{dog.life_span}</p>
+
+              <div className="column is-two-thirds-desktop is-light">
+                <h4 className="title is-4">
+                  Breed Group: <span>{dog.breed_group}</span>
+                </h4>
+                <hr id="hr1" />
+
+                <h4 className="title is-4">
+                  Lifespan: <span>{dog.life_span}</span>
+                </h4>
+
                 <h4 className="title is-4">Temperament:</h4>
-                <hr />
+
                 <p>{dog.temperament}</p>
               </div>
             </div>
