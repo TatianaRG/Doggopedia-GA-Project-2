ee73ae75ca53770a4889edbded9efdcd697a6f31
Matt Creese <mattycreese@gmail.com>
2022-03-11 12:15:21 +0000

    padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,13 @@ const Home = () => {
   return (
     <section className='hero is-fullheight-with-navbar is-primary'>
       <div className='hero-body'>
-        <div className='container'>
-          <p className='title has-text-centered '></p>
+        <div className='home-container'>
+          <h3 className='title has-text-centered '>
+            Welcome to Doggypedia, the encyclopaedia for dogs. Here at
+            Doggypedia we aim to make it easy and accessible for you to fully
+            research different dog breeds so you will feel comfortable when
+            choosing your next furry, four legged companion!
+          </h3>
         </div>
       </div>
     </section>
